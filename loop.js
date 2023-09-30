ce8b98c5435d19e112faae94dd246ded78087fed
= <=>
2023-09-30 12:58:59 +0100

    javascript challenge

---
+++ loop.js
@@ -0,0 +1,26 @@
+
+for (let i =0; i<10; i++){
+    console.log("hello world")
+}
+
+count = 0;
+
+
+
+
+
+
+
+//////////////////////////////////////////////
+
+let today = "saturday"
+
+if (today=="saturday") {
+    console.log("come to school")
+} else if (today=="sunday")  {
+    console.log("sleep")
+} else if (today=="monday"){
+    console.log("go to school")
+}else{
+    console.log("do whatever you want")
+}
